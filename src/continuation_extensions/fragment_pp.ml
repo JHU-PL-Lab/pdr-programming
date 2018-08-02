8cacfaaacfcc334ab1446d77e98058944597f9f3
Zachary Palmer <zep_git@bahj.com>
2018-08-02 16:49:24 -0400

    Working sandbox application with pretty-printing.

---
+++ src/continuation_extensions/fragment_pp.ml
@@ -0,0 +1,149 @@
+(** This module provides pretty-printing routines for visualizing the fragment
+    types.  Note that these routines invoke the code functions of the fragments;
+    they should largely be used for debugging only. *)
+
+open Format;;
+open Parsetree;;
+open Pprintast;;
+
+open Batteries;;
+
+open Jhupllib;;
+open Pp_utils;;
+
+open Pdr_programming_utils.Variable_utils;;
+
+open Fragment_types;;
+
+let box = pp_open_box;;
+let ch = pp_print_char;;
+let close = pp_close_box;;
+let cut = pp_print_cut;;
+let fspace = fun formatter () -> pp_print_char formatter ' ';;
+let hvbox = pp_open_hvbox;;
+let space = pp_print_space;;
+let str = pp_print_string;;
+let vbox = pp_open_vbox;;
+
+let record_start f () =
+  vbox f 0;
+  str f "{ ";
+  vbox f 0;
+  cut f ();
+;;
+
+let record_stop f () =
+  close f ();
+  cut f ();
+  ch f '}';
+  close f ();
+;;
+
+let record_item ?first:(first=false) f name value_printer value =
+  if not first then space f ();
+  hvbox f 2;
+  str f name;
+  fspace f ();
+  ch f '=';
+  space f ();
+  value_printer f value;
+  ch f ';';
+  close f ();
+;;
+
+let pp_input_hole_data : input_hole_data pretty_printer = fun f ihd ->
+  ignore ihd;
+  str f "INPUT_HOLE";
+;;
+
+let pp_evaluation_hole_data : evaluation_hole_data pretty_printer = fun f ehd ->
+  record_start f ();
+  record_item ~first:true f "target_fragment" (pp_option Fragment_uid.pp)
+    ehd.evhd_target_fragment;
+  record_item f "bound_variables" (Var_map.pp @@ pp_option core_type)
+    ehd.evhd_bound_variables;
+  record_stop f ();
+;;
+
+let pp_extension_hole_data : extension_hole_data pretty_printer = fun f ehd ->
+  record_start f ();
+  record_item ~first:true f "extension" expression
+    { pexp_desc = Pexp_extension ehd.exhd_extension;
+      pexp_loc = ehd.exhd_loc;
+      pexp_attributes = []
+    };
+  record_item f "target_fragment" (pp_option Fragment_uid.pp)
+    ehd.exhd_target_fragment;
+  record_item f "bound_variables" (Var_map.pp @@ pp_option core_type)
+    ehd.exhd_bound_variables;
+  record_stop f ();
+;;
+
+let pp_fragment : fragment pretty_printer = fun f frag ->
+  record_start f ();
+  record_item ~first:true f "uid" Fragment_uid.pp frag.fragment_uid;
+  record_item f "free_vars" Var_set.pp frag.fragment_free_variables;
+  record_item f "ext_bound_vars"
+    (Var_map.pp @@ pp_tuple Fragment_uid.pp @@ pp_option core_type)
+    frag.fragment_externally_bound_variables;
+  record_item f "input_hole" (pp_option pp_input_hole_data)
+    frag.fragment_input_hole;
+  record_item f "eval_holes" (pp_list pp_evaluation_hole_data)
+    frag.fragment_evaluation_holes;
+  record_item f "ext_holes" (pp_list pp_extension_hole_data)
+    frag.fragment_extension_holes;
+  record_item f "code" expression (
+    frag.fragment_code
+      (
+        if Option.is_some frag.fragment_input_hole then
+          Some [%expr INPUT_HOLE ]
+        else
+          None
+      )
+      (
+        frag.fragment_evaluation_holes
+        |> List.map (fun ehd expr ->
+            let uid_str =
+              ehd.evhd_target_fragment
+              |> Option.map Fragment_uid.show
+              |> Option.default "None"
+            in
+            let uid_str_expr =
+              { pexp_desc = Pexp_constant(Pconst_string(uid_str,None));
+                pexp_loc = ehd.evhd_loc;
+                pexp_attributes = [];
+              }
+            in
+            [%expr EVAL_HOLE([%e uid_str_expr], [%e expr])]
+          )
+      )
+      ( frag.fragment_extension_holes
+        |> List.map (fun ehd ->
+            let uid_str =
+              ehd.exhd_target_fragment
+              |> Option.map Fragment_uid.show
+              |> Option.default "None"
+            in
+            let uid_str_expr =
+              { pexp_desc = Pexp_constant(Pconst_string(uid_str,None));
+                pexp_loc = ehd.exhd_loc;
+                pexp_attributes = [];
+              }
+            in
+            [%expr EXT_HOLE([%e uid_str_expr])]
+          )
+      )
+  );
+  record_stop f ();
+;;
+
+let pp_fragment_group : fragment_group pretty_printer = fun f fg ->
+  record_start f ();
+  record_item ~first:true f "graph"
+    (pp_map Fragment_uid.pp pp_fragment Fragment_uid_map.enum)
+    fg.fg_graph;
+  record_item f "entry" Fragment_uid.pp fg.fg_entry;
+  record_item f "exits" (pp_set Fragment_uid.pp Fragment_uid_set.enum)
+    fg.fg_exits;
+  record_stop f ();
+;;
--- test/sandbox.ml
+++ test/sandbox.ml
@@ -2,90 +2,25 @@ open Batteries;;
 open Jhupllib;;
 
 open Asttypes;;
-open Continuation_fragment_types;;
-open Parsetree;;
-open Printf;;
 
-let debug_render_fragment (f : fragment) : string =
-  let input_expr_opt =
-    match f.fragment_input_hole with
-    | None -> None
-    | Some _ -> Some ([%expr _INPUT])
-  in
-  let eval_hole_fns =
-    f.fragment_evaluation_holes
-    |> List.map
-      (fun eval_hole_data e ->
-         let uid_str =
-           eval_hole_data.evhd_target_fragment
-           |> Option.map Fragment_uid.show
-           |> Option.default "None"
-         in
-         let uid_str_expr =
-           { pexp_desc = Pexp_constant(Pconst_string(uid_str,None));
-             pexp_loc = eval_hole_data.evhd_loc;
-             pexp_attributes = [];
-           }
-         in
-         [%expr EVAL_HOLE([%e uid_str_expr],[%e e])]
-      )
-  in
-  let ext_hole_exprs =
-    f.fragment_extension_holes
-    |> List.map
-      (fun ext_hole_data ->
-         let uid_str =
-           ext_hole_data.exhd_target_fragment
-           |> Option.map Fragment_uid.show
-           |> Option.default "None"
-         in
-         let uid_str_expr =
-           { pexp_desc = Pexp_constant(Pconst_string(uid_str,None));
-             pexp_loc = ext_hole_data.exhd_loc;
-             pexp_attributes = [];
-           }
-         in
-         [%expr EXT_HOLE([%e uid_str_expr])]
-      )
-  in
-  let e = f.fragment_code input_expr_opt eval_hole_fns ext_hole_exprs in
-  let expr_str = Pprintast.string_of_expression e in
-  sprintf "===== #%s =====\n%s" (Fragment_uid.show f.fragment_uid) expr_str
-;;
-
-let debug_render_fragment_group (g : fragment_group) : string =
-  let entry_string = sprintf "Entry: %s" (Fragment_uid.show g.fg_entry) in
-  let exit_string =
-    sprintf "Exits: %s"
-      (Pp_utils.pp_to_string Fragment_uid_set.pp g.fg_exits) in
-  let fragments_string =
-    Fragment_uid_map.values g.fg_graph
-    |> Enum.fold
-      (fun s f ->
-         s ^ (if s = "" then "" else "\n") ^ debug_render_fragment f
-      ) ""
-  in
-  sprintf "%s\n%s\n%s" entry_string exit_string fragments_string
-;;
+open Pdr_programming_continuation_extensions;;
+open Pdr_programming_utils;;
 
 let main () =
-  if Array.length Sys.argv != 2 then begin
-    prerr_endline "Expected exactly one argument.";
-    Pervasives.exit 1
-  end;
-  let source = Sys.argv.(1) in
-  let lexbuf = Lexing.from_string source in
-  let ast = Parse.expression lexbuf in
-  ast
-  |> Continuation_transformer.do_transform
-  |> Continuation_transformer_monad.run
-    (Continuation_fragment_types.Fragment_uid.new_context ())
-    (Variable_utils.new_fresh_variable_context ~prefix:"var" ())
-    (fun (name,payload) ->
-       name.txt = "pop"
-    )
-  |> debug_render_fragment_group
-  |> print_endline
+  let expr = [%expr
+     (* if [%pop] then 4 else 5 *)
+    4
+  ]
+  in
+  let result =
+    expr
+    |> Transformer.do_transform
+    |> Transformer_monad.run
+      (Fragment_types.Fragment_uid.new_context ())
+      (Variable_utils.new_fresh_variable_context ~prefix:"var" ())
+      (fun (name,_) -> name.txt = "pop")
+  in
+  print_endline @@ Pp_utils.pp_to_string Fragment_pp.pp_fragment_group result
 ;;
 
 main ();;
