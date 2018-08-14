f461b7da6cc413b41559d1afbd74a2493fecadbe
Zachary Palmer <zep_git@bahj.com>
2018-08-14 17:27:19 -0400

    Corrected logic to handle cases in which %transition does not appear at top level.

--- src/ppx/mapper.ml
+++ src/ppx/mapper.ml
@@ -15,14 +15,19 @@ let transform_structure_item mapper structure_item : structure =
     begin
       match body with
       | [ { pstr_desc = Pstr_value(_, [binding]); pstr_loc = _; } ] ->
-        Continuation_code.generate_code_from_function binding.pvb_expr
+        let s = Continuation_code.generate_code_from_function binding.pvb_expr in
+        prerr_string (Jhupllib.Pp_utils.pp_to_string (Printast.structure 0) s);
+           prerr_string "\n";
+           prerr_string (Jhupllib.Pp_utils.pp_to_string Pprintast.structure s);
+           prerr_string "\n";
+        s
       | _ ->
         raise @@ Location.Error(
           Location.error
             ~loc:structure_item.pstr_loc @@
           "transition extension must be applied to a single let binding")
     end
-  | _ -> [structure_item]
+  | _ -> [structure_item']
 ;;
 
 let mapper =
--- test/ppx_test.ml
+++ test/ppx_test.ml
@@ -1,4 +1,7 @@
-let%transition foo (a : int) =
-  let x = [%pop] in
-  (a,x)
+module Foo = struct
+  let%transition foo (a : int) =
+    let x = [%pop] in
+    (a,x)
+  ;;
+end
 ;;
